97ef0bdd1508ed0daa84ccab37b3b39085339cf3
Sayed Fazilat Fatima <fazilatfatima313@gmail.com>
2026-03-15 12:22:14 +0530

    Delete Face2Hire directory

--- Face2Hire/test.py
+++
@@ -1,9 +0,0 @@
-import google.generativeai as genai
-
-# Directly paste your Gemini API key here for testing
-genai.configure(api_key="AIzaSyAo8DvhIs-ySpPx6DZh32kyDK7dY-UO5hM")
-
-
-
-for m in genai.list_models():
-    print(m.name)
